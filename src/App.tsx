3935f7ac1f079f15c4abc1f0b0729336c1dccd03
Luis de Santiago <luis.desantiago@vista.com>
2023-04-05 10:01:34 +0200

    Added newrelic browser agent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent'
+
 
 function App() {
   return (
